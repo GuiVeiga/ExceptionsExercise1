1ebc24dc72dc2e385f61f07c073d3c3035c3d4f6
Guilherme Veiga <guilhermeveigarj@hotmail.com>
2019-08-19 15:06:58 -0300

    RuntimeException added

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,31 +11,35 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter account data");
-        System.out.print("Number: ");
-        int number = sc.nextInt();
-        System.out.print("Holder: ");
-        sc.nextLine();
-        String holder = sc.nextLine();
-        System.out.print("Initial balance: ");
-        double balance = sc.nextDouble();
-        System.out.print("Withdraw limit: ");
-        double withdrawLimit = sc.nextDouble();
-
-        Account acc = new Account(number, holder, balance, withdrawLimit);
-
-        System.out.println();
-        System.out.print("Enter the amount to withdraw: ");
-        double withdrawValue = sc.nextDouble();
-
         try {
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            System.out.print("Holder: ");
+            sc.nextLine();
+            String holder = sc.nextLine();
+            System.out.print("Initial balance: ");
+            double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double withdrawLimit = sc.nextDouble();
+
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            System.out.println();
+            System.out.print("Enter the amount to withdraw: ");
+            double withdrawValue = sc.nextDouble();
+
             acc.withdraw(withdrawValue);
             System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
 
         } catch (DomainException e) {
             System.out.println("Withdraw error: " + e.getMessage());
 
+        } catch (RuntimeException e) {
+            System.out.println("Unexpected error");
+
         }
+
         sc.close();
     }
 }
